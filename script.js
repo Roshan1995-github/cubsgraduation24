c7cb4c0517293b92c71ba252d5c0890a44ea25ea
Roshan Manohar <roshanmanohar.10v@gmail.com>
2024-01-28 13:00:07 +0800

    first full code commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("imported")
+
+function databaseLink() {
+	var databaseLink = 'http://cubsseatbooking.test';
+	// var databaseLink = 'https://cubsgraduation24-14c569002e4e.herokuapp.com';
+
+	return databaseLink;
+}
+
+
+function changeDisplay(elmnt, styl) {
+	elmnt.style.display = styl;
+}
+
+function logout() {
+	sessionStorage.removeItem("token")
+	window.location.replace("login.html")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
